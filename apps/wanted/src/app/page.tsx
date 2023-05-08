0810df3d6a9f9f15e3db0bf620299ded6b9266d6
윤현정 <yoonhj97@naver.com>
2023-05-08 14:44:36 +0900

    [ADD] typecheck scripts 추가하기

--- apps/wanted/src/app/page.tsx
+++ apps/wanted/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
-        <Button>{sayHello()}</Button>
+        <Button variant="contained">{sayHello()}</Button>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
